b0aa15282f8d822799d58aa8ea3c44a3340167ad
KuebV <rwthompson01@bvsd.org>
2021-07-26 01:51:33 -0600

    Inital, and probably last commit

---
+++ ColorfulConsole/ColorfulConsole/ColorfulConsole.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ColorfulConsole
+{
+    public class ColorfulConsole
+    {
+        public static void WriteLine(string text, ConsoleColor? TextColor = null, bool stayColored = false)
+        {
+            if (text == null) return;
+            if (TextColor != null)
+            {
+                Console.ForegroundColor = TextColor.Value;
+            }
+            Console.WriteLine(text);
+
+            if (!stayColored)
+                Console.ForegroundColor = ConsoleColor.White;
+
+        }
+
+        public static void Write(string text, ConsoleColor? TextColor = null, bool stayColored = false)
+        {
+            if (text == null) return;
+            if (TextColor != null)
+            {
+                Console.ForegroundColor = TextColor.Value;
+            }
+            Console.Write(text);
+
+            if (!stayColored)
+                Console.ForegroundColor = ConsoleColor.White;
+        }
+    }
+}
---
+++ ColorfulConsole/ColorfulConsole/lOG.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ColorfulConsole
+{
+    public class Log
+    {
+        public static void Info(string text) => ColorfulConsole.WriteLine(text, ConsoleColor.Cyan);
+        public static void Error(string text) => ColorfulConsole.WriteLine(string.Format("[Error] {0}", text), ConsoleColor.Red);
+        public static void Debug(string text) => ColorfulConsole.Write(string.Format("[Debug] {0}", text), ConsoleColor.Yellow);
+    }
+}
